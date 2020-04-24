cb721b182c8706d8d59417f54e05a2b974225d67
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-04-24 11:20:49 -0400

    progress on questions

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-const questions = [
-    "What's the title of your project?",
-    "Write a brief description (2-3 sentences)",
-    "What's your GitHub username?",
-    "Please write a brief how-to install",
-    "How do you use it?",
-    "License",
-    "Contributers",
-    "Tests",
-    "FAQ"
-];
+let fs = require('fs');
 
 function writeToFile(fileName, data) {
-
+    fs.writeFile(fileName, data, writeToFile);
+    // Write user input in file
 }
 
-function init() {
 
-    // List question1
-    // get user input from answer
-    // store in data
-    // grab q 2
-    // LOOP ^^^
-    // Write answers to file
-    // at the end of Write to file, generate and place new file in package
-
+async function fun1(req, res) {
+    const { prompt } = require('enquirer');
+    const response = await prompt ([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What\'s the project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Type a brief project description.',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install it?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use it?',
+    }, 
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Are there any licenses?',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test it?',
+    }, {
+        type: 'input',
+        name: 'questions',
+        message: 'Any FAQ or additional readings?',
+    }
+    ]);
+    // console.log(response);
+    data = response;
+    writeToFile('readme.md', data);
 }
 
-init();
+fun1();
+
