0e62799341d3d0ed95b7b564552ac033cc6e625a
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-04-24 10:06:54 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const questions = [
+    "What's the title of your project?",
+    "Write a brief description (2-3 sentences)",
+    "What's your GitHub username?",
+    "Please write a brief how-to install",
+    "How do you use it?",
+    "License",
+    "Contributers",
+    "Tests",
+    "FAQ"
+];
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+
+    // List question1
+    // get user input from answer
+    // store in data
+    // grab q 2
+    // LOOP ^^^
+    // Write answers to file
+    // at the end of Write to file, generate and place new file in package
+
+}
+
+init();
